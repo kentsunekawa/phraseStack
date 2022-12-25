9c0a4ab218dd2effb4090b83a150d206ee8a5d96
kentsunekawa <dev@k-tsunekawa.com>
2022-12-25 17:25:17 +0900

    色変更

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,14 @@
 import { createTheme } from '@mui/material/styles'
 
-export const muiTheme = createTheme()
+export const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#008B94',
+      light: '#00B8C3',
+      dark: '#005358',
+    },
+  },
+})
 
 export const combinedDefaultTheme = {
   ...muiTheme,
